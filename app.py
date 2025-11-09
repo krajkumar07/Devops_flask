f6ff5a0312c68c6ce4c53792e12b3ad5b26f6c7d
krajkumar07 <kumarr60303@gmail.com>
2025-11-09 11:02:02 +0530

    added files

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask , request , render_template, jsonify
+from datetime import datetime
+import json
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    day_of_week = datetime.today().strftime('%A')
+    return render_template('index.html', day_of_week=day_of_week)
+
+@app.route('/name')
+def fun():
+    name=request.values.get('name')
+    age=request.values.get('age')
+    restult={
+        'name': name,
+         'age':age
+    }
+    return restult
+
+@app.route('/api', methods=['GET'])
+def get_data():
+    with open('data.json', 'r') as f:
+        data = json.load(f)   
+    return jsonify(data)      
+
+
+if __name__=='__main__':
+    app.run(debug=True)
